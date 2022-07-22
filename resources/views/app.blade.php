fc118f776832f4f489bf1ee9b97e99dff380200c
Lidia <lidava@gmail.com>
2022-07-22 22:46:55 +0400

    add cosmetic site height

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,7 +14,7 @@
         @vite('resources/js/app.js')
         @inertiaHead
     </head>
-    <body class="font-sans">
+    <body class="font-sans antialiased h-full">
         @inertia
     </body>
 </html>
