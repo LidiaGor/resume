91d8c0ca4c3d058b723c04649fb6094a975d70ce
Lidia <lidava@gmail.com>
2022-07-22 23:04:45 +0400

    add header visual

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,11 +1,25 @@
 <template>
 
-    <header class="py-3">
-        header
+    <header class="py-3 bg-blue-100">
+        <div class=" sm:flex max-w-screen-xl mx-auto">
+            <NavLink
+                :href="route('home.index')"
+                :active="route().current('dashboard')"
+            >
+                Dashboard
+            </NavLink>
+            <Link
+                :href="route('home.index')"
+            >
+                Dashboard
+            </Link>
+        </div>
     </header>
 
     <main class="">
-        <slot/>
+        <div class="max-w-screen-xl mx-auto">
+            <slot/>
+        </div>
     </main>
 
     <footer class="mt-auto py-3 bg-gray-300 border-t border-gray-800">
@@ -15,5 +29,7 @@
 </template>
 
 <script setup>
+import NavLink from '@/Components/NavLink.vue';
+import {Link} from "@inertiajs/inertia-vue3";
 
 </script>
