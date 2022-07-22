6bb94d2312f58f720b37df47c3825480ff0b810e
Lidia <lidava@gmail.com>
2022-07-22 22:45:09 +0400

    initial commit

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <header class="py-3">
+        header
+    </header>
+
+    <main class="">
+        <slot/>
+    </main>
+
+    <footer class="mt-auto py-3 bg-gray-300 border-t border-gray-800">
+        footer
+    </footer>
+
+</template>
+
+<script setup>
+
+</script>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <AppLayout>
+        123132
+    </AppLayout>
+</template>
+
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue'
+
+const
+    props = defineProps({
+
+    })
+</script>
